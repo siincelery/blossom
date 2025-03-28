d9db73b7f12452dabdbf7f42b4f974d003dd9e7b
blos <alinaavalieva@vk.com>
2025-03-28 23:06:20 +0500

    registration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.template import context
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
     
@@ -27,8 +27,18 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+                form.save()
+                user=form.instance
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context ={
         'title':'Регистрация',
+        'form':form,
     }
     return render(request, 'users/registration.html', context)
 
@@ -38,5 +48,7 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
